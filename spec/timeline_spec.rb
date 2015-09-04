a6bee31aef298bee4b37b20de933fe40163e41e1
myun2 <myun2@nwhite.info>
2015-09-04 17:10:00 +0900

    init timeline gem.

---
+++ spec/timeline_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Timeline do
+  it 'has a version number' do
+    expect(Timeline::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
